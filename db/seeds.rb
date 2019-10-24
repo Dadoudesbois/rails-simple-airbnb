a2cab8334fa190d2a1e3b42ff87e74bc38cece95
Daphne Laboul <daphne.laboul@gmail.com>
2019-10-24 12:50:21 +0200

    seeded flats, model, controller, routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,17 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
+require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+6.times do
+  Flat.create!(
+    name: Faker::GreekPhilosophers.unique.name,
+    address: Faker::Address.unique.full_address,
+    description: Faker::GreekPhilosophers.unique.quote,
+    price_per_night: rand(35..190),
+    number_of_guests: rand(2..10),
+  )
+end
+
+
